1c227e5eef1648385fe720b450672dd36aac5e5b
João André Marques <joaoandremarques@gmail.com>
2026-09-04 22:41:40 +0100

    feat: file reading function

--- Old Exams - Solutions/Exame 2025/src/posts.c
+++ Old Exams - Solutions/Exame 2025/src/posts.c
@@ -4,3 +4,14 @@
 
 #include "../include/types.h"
 #include "../include/posts.h"
+
+typedef struct {
+    
+} Post;
+
+typedef struct {
+
+} PostList;
+
+Status post_list_add(PostList *list, int id, const char *description, const char *influencer, const char *category);
+Status post_list_update_metrics(PostList *list, int id, int views, int likes, int comments);
